0ded099d09ce02dcf434e2367a6880164db2e0d3
leletor <601122452@qq.com>
2022-12-24 18:39:17 +0800

    lab12

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -71,9 +71,9 @@ public class MergeSort {
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
         // Your code here!
-        Queue<Queue<Item>> queues = makeSingleItemQueues(items);
         if (items.isEmpty())
             return null;
+        Queue<Queue<Item>> queues = makeSingleItemQueues(items);
         while (queues.size() != 1){
             Queue<Item> q1 = queues.dequeue();
             Queue<Item> q2 = queues.dequeue();
